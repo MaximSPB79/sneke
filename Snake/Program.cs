86db439f4001833635c591c4bf514e29d84996d9
MaximSPB79 <ozmaxim@gmail.com>
2022-01-09 02:24:49 +0300

    Создали класс фигура

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,12 +10,15 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
+          //  Console.SetBufferSize(width: 80, height: 25);
             HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine dounLine = new HorizontalLine(0, 78, 24, '+');
-            V
+            VLine leftLine = new VLine (0,24,0,'+');
+            VLine rightLine = new VLine(0,24,78,'+');
             upLine.Drow();
             dounLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
 
            Console.ReadLine();
         }
--- Snake/VLine.cs
+++ Snake/VLine.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,21 +6,19 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HorizontalLine:Figure
+     class VLine:Figure
     {
-        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+
+        public VLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xRight; x++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-            
+
         }
-        
+       
     }
 }
-             
-    
-
